f1e65213fb26083ff32f6d2e104989cb94a10718
Chris Clarke <chris.m.clarke@live.co.uk>
2018-03-12 13:33:56 +0000

    Update and rename environment.html to environment.js

---
+++ src/environment/environment.js
@@ -0,0 +1,27 @@
+
+// update this file with your own firebase configuration details
+// more info available at: https://firebase.google.com/docs/web/setup
+// additional analytics field key can be populated if you are registered for google analytics
+
+// if you would like to use multiple different settings define them here. Otherwise just copy your settings into the production field
+
+var configurations = {
+    production: {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        analyticsKey: ""
+    },
+    staging: {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "m",
+        messagingSenderId: "",
+        analyticsKey: ""
+    }
+}
