26419b05a45abd729c0808f863958bd47b2a319a
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-03-12 13:46:42 +0000

    set master branch to production

--- src/environment/setConfig.js
+++ src/environment/setConfig.js
@@ -1,5 +1,6 @@
 // use this file if you wish to set an alternate environment configuration (e.g. staging or production)
 
+// set master branch to production
 var currentEnvironment = "production"
 console.log('ENVIRONMENT:',currentEnvironment)
 window['firebaseConfig']=configurations[currentEnvironment]
