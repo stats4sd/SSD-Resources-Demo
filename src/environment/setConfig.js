83144366ce7a636d7e29595709a0edb222c6afa1
laptop\chris <chris.m.clarke@live.co.uk>
2018-06-18 17:24:46 -0700

    fix missing icons, add redux bindings for active resource and collection, fix resource not resetting bug, build out collection editor, some code tidying

--- src/environment/setConfig.js
+++ src/environment/setConfig.js
@@ -1,6 +1,9 @@
 // use this file if you wish to set an alternate environment configuration (e.g. staging or production)
 
-// set master branch to production
-var currentEnvironment = "staging"
-console.log('ENVIRONMENT:',currentEnvironment)
-window['firebaseConfig']=configurations[currentEnvironment]
+// set triggers to automatically change branch if desired
+var currentEnvironment = "staging";
+if (location.host == "resources.stats4sd.org") {
+  currentEnvironment = "production";
+}
+console.log("ENVIRONMENT:", currentEnvironment);
+window["firebaseConfig"] = configurations[currentEnvironment];
